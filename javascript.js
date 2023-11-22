a37ccf88ccc586f718301ed9a85f63c2ac8a8343
migena <migena.allushi1@gmail.com>
2023-11-22 08:51:09 +0100

    eventlistener to change the color of the divs

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 let gridContainer = document.getElementById('grid-container');
 
 let n = prompt("Type a number between 0-64?");
+let squareSize = 500 / n - 2;
+
 for (i=0; i < n; i++){
     let squareRow = document.createElement('div');
     squareRow.className = 'row';
@@ -8,6 +10,13 @@ for (i=0; i < n; i++){
     for(j=0; j < n; j++){
         let squareCol = document.createElement('div');
         squareCol.className = 'col';
+        squareCol.style.width = `${squareSize}px`;
+        squareCol.style.height = `${squareSize}px`;
+
+        squareCol.addEventListener('mouseover', function() {
+            squareCol.style.backgroundColor = 'white';
+        })
+
         squareRow.appendChild(squareCol);
     }
     gridContainer.appendChild(squareRow);
